d66ad69eaae9801689c7decf66a383ecc570b86e
colorzx0716 <31384907+colorzx0716@users.noreply.github.com>
2017-10-25 19:51:19 +0800

    Initial commit

---
+++ app/src/main/java/com/bawie/mygouwuche5/ProductBean.java
@@ -0,0 +1,35 @@
+package com.bawie.mygouwuche5;
+
+/**
+ * Created by 张肖肖 on 2017/10/25.
+ */
+
+import java.util.List;
+
+/**
+ * Created by 苏照亮 on 2017/10/25.
+ */
+
+public class ProductBean {
+
+    public String sellerName;
+    public List<DataBean> list;
+
+    public ProductBean(String sellerName, List<DataBean> list) {
+        this.sellerName = sellerName;
+        this.list = list;
+    }
+
+    public static class DataBean{
+        public DataBean(String producrName, double price, boolean isSelect) {
+            this.producrName = producrName;
+            this.price = price;
+            this.isSelect = isSelect;
+        }
+
+        public String producrName;
+        public double price;
+        public boolean isSelect;
+    }
+
+}
